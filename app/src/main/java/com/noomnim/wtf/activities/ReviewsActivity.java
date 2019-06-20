8abcce3a60508a4cf55860a5a60e8662c5e288c2
MasterRueangyotTreemat <ruengyod1@gmail.com>
2019-06-20 23:48:11 +0700

    Login Activity

--- app/src/main/java/com/noomnim/wtf/activities/ReviewsActivity.java
+++ app/src/main/java/com/noomnim/wtf/activities/ReviewsActivity.java
@@ -2,11 +2,17 @@ package com.noomnim.wtf.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.noomnim.wtf.R;
+import com.noomnim.wtf.adapter.FoodTruckAdapter;
+import com.noomnim.wtf.adapter.ReviewAdapter;
 import com.noomnim.wtf.data.DataService;
 import com.noomnim.wtf.model.FoodTruck;
 import com.noomnim.wtf.model.FoodTruckReview;
+import com.noomnim.wtf.view.ItemDecorator;
 
 import java.util.ArrayList;
 
@@ -15,6 +21,8 @@ public class ReviewsActivity extends AppCompatActivity {
     // Variable
     private FoodTruck foodTruck;
     private ArrayList<FoodTruckReview> reviews = new ArrayList<>(  );
+    private ReviewAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
@@ -27,14 +35,32 @@ public class ReviewsActivity extends AppCompatActivity {
             @Override
             public void success(Boolean success) {
                 if(success){
-                    System.out.println( reviews );
+                    //System.out.println( reviews );
+                    setUpRecycler();
+                    if(reviews.size() == 0) {
+                        Toast.makeText( getBaseContext(), "No reviews for this Food Truck", Toast.LENGTH_SHORT ).show();
+                    }
                 }
             }
         };
         reviews = DataService.getInstance().downloadReviews( this, foodTruck, listener );
     }
 
+
+
     public interface ReviewInterface{
         void success(Boolean success);
+    }
+
+    private void setUpRecycler(){
+        RecyclerView recyclerView = (RecyclerView) findViewById( R.id.recycler_reviews );
+        recyclerView.setHasFixedSize( true );
+        adapter = new ReviewAdapter( reviews );
+        recyclerView.setAdapter( adapter );
+        LinearLayoutManager layoutManager = new LinearLayoutManager( getBaseContext() );
+        layoutManager.setOrientation( LinearLayoutManager.VERTICAL );
+        recyclerView.setLayoutManager( layoutManager );
+        recyclerView.addItemDecoration( new ItemDecorator( 10,10,10,10 ) );
+
     }
 }
---
+++ app/src/main/java/com/noomnim/wtf/adapter/ReviewAdapter.java
@@ -0,0 +1,40 @@
+package com.noomnim.wtf.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.noomnim.wtf.R;
+import com.noomnim.wtf.holder.ReviewHolder;
+import com.noomnim.wtf.model.FoodTruckReview;
+
+import java.util.ArrayList;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewHolder> {
+
+    private ArrayList<FoodTruckReview> reviews;
+
+    public ReviewAdapter(ArrayList<FoodTruckReview> reviews){
+        this.reviews = reviews;
+    }
+
+    @NonNull
+    @Override
+    public ReviewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View reviewCard = LayoutInflater.from( viewGroup.getContext() ).inflate( R.layout.card_review, viewGroup, false );
+        return new ReviewHolder( reviewCard );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReviewHolder reviewHolder, int i) {
+        final FoodTruckReview review = reviews.get( i );
+        reviewHolder.updateUI( review );
+    }
+
+    @Override
+    public int getItemCount() {
+        return reviews.size();
+    }
+}
